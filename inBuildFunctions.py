80fb48f032cef96bc12cc45b6ea972d0f3b9276f
bhavik <sirohiyabhavik@gmail.com>
2026-02-27 11:32:00 +0530

    Basics completed

---
+++ inBuildFunctions.py
@@ -0,0 +1,26 @@
+a = [1, 2, 3, 1, 4, 1]
+
+len(a)
+id(a)
+type(a)
+
+abs(-400)
+round(2.4)
+round(10.2345, 2)
+round(2.5) # Exception - round off to nearest even no.
+
+max(a)
+min(a)
+
+sum(a) 
+
+a.index(1, 2)
+a.count(1)
+
+b = "हिन्दी"
+print(b)
+
+ord("a") # for finding ascii value
+
+s1 = {12, 13, 11}
+print(s1)
---
+++ tasks.py
@@ -0,0 +1,8 @@
+# a = int(input("Enter a num"))
+# if len(str(a)) != 3:
+#     print(False)
+# elif 
+
+
+b = input("Enter")
+print(len(b) > 5)
